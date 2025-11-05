2166086f114c19ebec4c1a7af08cd3fd16544e32
Dierk Koenig <dierk.koenig@canoo.com>
2025-11-05 11:01:34 +0100

    self-made action redirecting the view to a materialized scaffold

--- grails-app/controllers/rooms/PersonController.groovy
+++ grails-app/controllers/rooms/PersonController.groovy
@@ -6,13 +6,14 @@ class PersonController {
     def probe() {
 
         // gib die Personen aus, die den Raum 5.2B51 gebucht haben
-//        Room xxx = Room.findByName("5.2B51")
-//        String result = Booking.findAllByRoom(xxx).booker
+        Room xxx = Room.findByName(params.room)
+        def result = Booking.findAllByRoom(xxx).booker
+        render view:"index", model:[personList: result]
 
         // Gib die Räume aus, die Roger gebucht hat
-        Person roger  = Person.findByFirstName("Roger")
-        String result = Booking.findAllByBooker(roger)
+//        Person roger  = Person.findByFirstName("Roger")
+//        String result = Booking.findAllByBooker(roger)
 
-        render text:result
+//        render text:result
     }
 }
