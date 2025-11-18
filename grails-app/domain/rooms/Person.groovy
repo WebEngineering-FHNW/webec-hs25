9bfaf3f2fb27cec7a311556fc0f5222eb1f75a77
Dierk Koenig <dierk.koenig@canoo.com>
2025-11-18 21:24:56 +0100

    make Person relate to SecUser

--- grails-app/domain/rooms/Person.groovy
+++ grails-app/domain/rooms/Person.groovy
@@ -5,7 +5,15 @@ class Person {
     String  lastName
     Integer yearOfBirth
 
+    SecUser secUser
+
     String toString() {
         return firstName + " " + lastName
+    }
+
+    static constraints = {
+        firstName nullable: true
+        lastName  nullable: true
+        secUser   nullable: true
     }
 }
